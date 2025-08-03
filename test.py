0bafb7c74b904f9b36cd2356f3e27bf7bf76b965
taiichi <taiichi930@gmail.com>
2025-08-03 14:01:52 +0900

    test

--- test.py
+++ test.py
@@ -10,15 +10,15 @@ number2 = st.number_input("次の数字を入力してください：", value=0)
 
 # 計算
 result_add = number1 + number2
-result_subtract = number1 - number2
-result_multiply = number1 * number2
-result_divide = None if number2 == 0 else number1 / number2
+#result_subtract = number1 - number2
+#result_multiply = number1 * number2
+#result_divide = None if number2 == 0 else number1 / number2
 
 # 結果を表示
 st.write(f"足し算の結果: {result_add}")
-st.write(f"引き算の結果: {result_subtract}")
-st.write(f"掛け算の結果: {result_multiply}")
-if result_divide is None:
-    st.write("割り算は0では計算できません。")
-else:
-    st.write(f"割り算の結果: {result_divide}")
+#st.write(f"引き算の結果: {result_subtract}")
+#st.write(f"掛け算の結果: {result_multiply}")
+#if result_divide is None:
+#    st.write("割り算は0では計算できません。")
+#else:
+#    st.write(f"割り算の結果: {result_divide}")
